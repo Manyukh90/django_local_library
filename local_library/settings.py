acfa1df07c895ea51fb3f52ad3841ad5077d8ee3
Manyukh90 <manyukh90@gmail.com>
2022-05-03 11:52:56 +0700

    update collectstatic_02

--- local_library/settings.py
+++ local_library/settings.py
@@ -127,6 +127,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
